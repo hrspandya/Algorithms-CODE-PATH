8106796cd7bb445b866f65c9ba063ff11757c830
Harsh Pandya <hrspandya@gmail.com>
2018-05-21 18:40:41 -0700

    algorithms

---
+++ codepath_course/1_Hashing/colorful_number/Colorful_Number.java
@@ -0,0 +1,10 @@
+package colorful_number;
+
+public class Colorful_Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
